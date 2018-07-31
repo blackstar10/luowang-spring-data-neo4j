2024bfe280c81c628fcdac3710236f2744269cee
Sun Qiu <sunqiu@cmss.chinamobile.com>
2018-07-31 18:54:53 +0800

    Human测试用例
    Code Source From: Self Code
    Description:
    Jira:  #
    市场项目编号（名称）:

--- src/test/java/movies/spring/data/neo4j/repositories/HumanRepositoryTest.java
+++ src/test/java/movies/spring/data/neo4j/repositories/HumanRepositoryTest.java
@@ -1,5 +1,8 @@
 package movies.spring.data.neo4j.repositories;
 
+import movies.spring.data.neo4j.common.GenderTypes;
+import movies.spring.data.neo4j.domain.Human;
+import movies.spring.data.neo4j.domain.IDCard;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,6 +10,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Collection;
 
 /**
  * Create on 2018/7/31 17:59
@@ -18,14 +23,27 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Transactional
 public class HumanRepositoryTest {
+    private final String idNumber1 = "809312801x";
     @Autowired
     private HumanRepository humanRepository;
     @Before
     public void setUp(){
-
+        Human human = new Human();
+        //todo 判断是否为空
+        Collection<Human> humans = humanRepository.getHumanByIdNumber(idNumber1);
+        if(humans != null && humans.size() > 0){
+            human = humans.iterator().next();
+        }
+        human.setName("testjava1");
+        human.setBirthday("1992-11-20");
+        //todo 不对,
+        // 测试 1. 能否写一个id对应一个human，2.尝试取出idNumber再更新idCard如果需要的话
+        human.addIdCard(new IDCard(idNumber1));
+        human.setGender(GenderTypes.getGenderType(1).getMessage());
+        humanRepository.save(human);
     }
     @Test
     public void testFindByIdNumber(){
-
+        Collection<Human> human = humanRepository.getHumanByIdNumber(idNumber1);
     }
 }
