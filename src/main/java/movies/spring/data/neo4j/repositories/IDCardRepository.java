b646ced47ab7bc45aa337d5d3f1e4d763a755f57
Sun Qiu <sunqiu@cmss.chinamobile.com>
2018-08-01 10:21:26 +0800

    HumanRepoTest Code 1 PASS
    Code Source From: Self Code
    Description:
    Jira:  #
    市场项目编号（名称）:

--- src/main/java/movies/spring/data/neo4j/repositories/IDCardRepository.java
+++ src/main/java/movies/spring/data/neo4j/repositories/IDCardRepository.java
@@ -3,6 +3,7 @@ package movies.spring.data.neo4j.repositories;
 import movies.spring.data.neo4j.domain.IDCard;
 import org.springframework.data.neo4j.repository.Neo4jRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Collection;
 
@@ -11,6 +12,7 @@ import java.util.Collection;
  *
  * @author sunqiu@cmss.chinamobile.com
  */
+@Repository
 public interface IDCardRepository extends Neo4jRepository<IDCard, Long> {
 
     Collection<IDCard> findIDCardByIdNumber(@Param("idNumber") String idNumber);
--- src/test/java/movies/spring/data/neo4j/repositories/HumanRepositoryTest.java
+++ src/test/java/movies/spring/data/neo4j/repositories/HumanRepositoryTest.java
@@ -3,15 +3,18 @@ package movies.spring.data.neo4j.repositories;
 import movies.spring.data.neo4j.common.GenderTypes;
 import movies.spring.data.neo4j.domain.Human;
 import movies.spring.data.neo4j.domain.IDCard;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
+
 
 /**
  * Create on 2018/7/31 17:59
@@ -21,29 +24,48 @@ import java.util.Collection;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EnableNeo4jRepositories("movies.spring.data.neo4j.repositories")
 @Transactional
 public class HumanRepositoryTest {
-    private final String idNumber1 = "809312801x";
+    private final String idNumber1 = "01283210381";
+    private final String name1 = "testjava1";
     @Autowired
     private HumanRepository humanRepository;
+
     @Before
-    public void setUp(){
+    public void setUp() {
         Human human = new Human();
         //todo 判断是否为空
         Collection<Human> humans = humanRepository.getHumanByIdNumber(idNumber1);
-        if(humans != null && humans.size() > 0){
+        if (humans != null && humans.size() > 0) {
             human = humans.iterator().next();
         }
-        human.setName("testjava1");
+        human.setName(name1);
         human.setBirthday("1992-11-20");
-        //todo 不对,
-        // 测试 1. 能否写一个id对应一个human，2.尝试取出idNumber再更新idCard如果需要的话
-        human.addIdCard(new IDCard(idNumber1));
         human.setGender(GenderTypes.getGenderType(1).getMessage());
+        // 测试 1. 能否写一个id对应一个human，2.尝试取出idNumber再更新idCard如果需要的话
+        if (human.getIdCardList().size() == 0) {
+            IDCard idCard1 = new IDCard(idNumber1);
+            human.addIdCard(idCard1);
+            idCard1.setHuman(human);
+        }
         humanRepository.save(human);
+        System.out.println(human.getId().toString());
+        Collection<Human> humans2 = humanRepository.getHumanByIdNumber(idNumber1);
+
+        System.out.println(humans2.size());
+    }
+
+    @Test
+    public void testFindByIdNumber() {
+        Collection<Human> humans = humanRepository.getHumanByIdNumber(idNumber1);
+        Assert.assertNotNull(humans);
+        Assert.assertNotEquals(0,humans.size());
     }
     @Test
-    public void testFindByIdNumber(){
-        Collection<Human> human = humanRepository.getHumanByIdNumber(idNumber1);
+    public void testFindByName() {
+        Collection<Human> humans = humanRepository.findByName(name1);
+        Assert.assertNotNull(humans);
+        Assert.assertNotEquals(0,humans.size());
     }
 }
